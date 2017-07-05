eba0b4b8672923547028d9d1b992402ec6786cfa
Scott O'Halloran <o_halloran@yahoo.com>
2017-07-04 21:29:13 -0700

    Update 2 4July17

---
+++ AdjectiveSection.swift
@@ -0,0 +1,23 @@
+//
+//  AdjectiveSections.swift
+//  IrishVocabulary
+//
+//  Created by Scott on 7/4/17.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import Foundation
+
+class Adjectiveection {
+    
+    
+    
+    var heading : String
+    var items : [String]
+    
+    init() {
+        heading = ""
+        items = []
+    }
+    
+}
---
+++ IrishVocabulary/NounSections.swift
@@ -0,0 +1,23 @@
+//
+//  NounSections.swift
+//  IrishVocabulary
+//
+//  Created by Scott on 7/4/17.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import Foundation
+
+class NounSection {
+    
+    
+    
+    var heading : String
+    var items : [String]
+    
+    init() {
+        heading = ""
+        items = []
+    }
+    
+}
