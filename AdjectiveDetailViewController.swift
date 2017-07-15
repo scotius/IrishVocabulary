d61da86b67eecce27955d4d4cd24051d58d633a8
Scott <o_halloran@yahoo.com>
2017-07-14 18:00:39 -0700

    Update 2 7/14/17

---
+++ AdjectiveDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AdjectiveDetailViewController.swift
+//  IrishVocabulary
+//
+//  Created by Scott on 7/14/17.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AdjectiveDetailViewController : UIViewController {
+
+    
+
+    
+    
+    
+}
